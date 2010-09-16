79cee6bb5424ff421b498c6c13b27e54d45d1bc3
watson@dcw.ca <watson@dcw.ca>
2010-09-16 14:41:56 -0400

    Rewrite, dumping __defineSetter__ and __defineSetter__

--- jsuri.js
+++ jsuri.js
@@ -23,7 +23,7 @@
 
 /*
     jsUri
-    version 1.0.2
+    version 1.1.0
 
     Uri parsing, manipulation and stringification.
 
@@ -77,84 +77,117 @@ jsUri.prototype.toString = function () {
     var s = '';
     var is = function (s) { return (s != null && s != ''); }
 
-    if (is(this.protocol)) {
-        s += this.protocol;
-        if (this.protocol.indexOf(':') != this.protocol.length - 1) {
+    if (is(this.protocol())) {
+        s += this.protocol();
+        if (this.protocol().indexOf(':') != this.protocol().length - 1) {
             s += ':';
         }
         s += '//';
     }
     else {
-        if (this.hasAuthorityPrefix && is(this.host))
+        if (this.hasAuthorityPrefix() && is(this.host()))
             s += '//';
     }
 
-    if (is(this.userInfo) && is(this.host)) {
-        s += this.userInfo;
-        if (this.userInfo.indexOf('@') != this.userInfo.length - 1)
+    if (is(this.userInfo()) && is(this.host())) {
+        s += this.userInfo();
+        if (this.userInfo().indexOf('@') != this.userInfo().length - 1)
             s += '@';
     }
 
-    if (is(this.host)) {
-        s += this.host;
-        if (is(this.port))
-            s += ':' + this.port;
+    if (is(this.host())) {
+        s += this.host();
+        if (is(this.port()))
+            s += ':' + this.port();
     }
 
-    if (is(this.path))
-        s += this.path;
+    if (is(this.path()))
+        s += this.path();
     else
-        if (is(this.host) && (is(this.query.toString()) || is(this.anchor)))
+        if (is(this.host()) && (is(this.query().toString()) || is(this.anchor())))
             s += '/';
 
-    if (is(this.query.toString())) {
-        if (this.query.toString().indexOf('?') != 0)
+    if (is(this.query().toString())) {
+        if (this.query().toString().indexOf('?') != 0)
             s += '?';
-        s += this.query.toString();
+        s += this.query().toString();
     }
 
-    if (is(this.anchor)) {
-        if (this.anchor.indexOf('#') != 0)
+    if (is(this.anchor())) {
+        if (this.anchor().indexOf('#') != 0)
             s += '#';
-        s += this.anchor;
+        s += this.anchor();
     }
 
     return s;
 }
 
+
 /*
-    Property accessor methods for jsUri
+    Basic get/set functions for all properties
 */
 
-jsUri.prototype.__defineGetter__('protocol', function () { return this._uri.protocol; });
-jsUri.prototype.__defineSetter__('protocol', function (val) { this._uri.protocol = val; });
+jsUri.prototype.protocol = function(val) {
+    if (typeof val != 'undefined') {
+        this._uri.protocol = val;
+    }
+    return this._uri.protocol;
+}
 
 // hasAuthorityPrefix: if there is no protocol, the leading // can be enabled or disabled
-jsUri.prototype.__defineGetter__('hasAuthorityPrefix', function () { 
+jsUri.prototype.hasAuthorityPrefix = function(val) {
+
+    if (typeof val != 'undefined') {
+        this._hasAuthorityPrefix = val;
+    }
+
     if (this._hasAuthorityPrefix == null)
-    return (this._uri.source.indexOf('//') != -1);
+        return (this._uri.source.indexOf('//') != -1);
+    else
+        return this._hasAuthorityPrefix;
+}
 
-    return this._hasAuthorityPrefix;
-});
-jsUri.prototype.__defineSetter__('hasAuthorityPrefix', function (val) {  this._hasAuthorityPrefix = val; });
+jsUri.prototype.userInfo = function(val) {
+    if (typeof val != 'undefined') {
+        this._uri.userInfo = val;
+    }
+    return this._uri.userInfo;
+}
 
-jsUri.prototype.__defineGetter__('userInfo', function () { return this._uri.userInfo; });
-jsUri.prototype.__defineSetter__('userInfo', function (val) { this._uri.userInfo = val; });
+jsUri.prototype.host = function(val) {
+    if (typeof val != 'undefined') {
+        this._uri.host = val;
+    }
+    return this._uri.host;
+}
 
-jsUri.prototype.__defineGetter__('host', function () { return this._uri.host; });
-jsUri.prototype.__defineSetter__('host', function (val) { this._uri.host = val; });
+jsUri.prototype.port = function(val) {
+    if (typeof val != 'undefined') {
+        this._uri.port = val;
+    }
+    return this._uri.port;
+}
 
-jsUri.prototype.__defineGetter__('port', function () { return this._uri.port; });
-jsUri.prototype.__defineSetter__('port', function (val) { this._uri.port = val; });
+jsUri.prototype.path = function(val) {
+    if (typeof val != 'undefined') {
+        this._uri.path = val;
+    }
+    return this._uri.path;
+}
 
-jsUri.prototype.__defineGetter__('path', function () { return this._uri.path; });
-jsUri.prototype.__defineSetter__('path', function (val) { this._uri.path = val; });
+jsUri.prototype.query = function(val) {
+    if (typeof val != 'undefined') {
+        this._query = new jsUri.query(val);
+    }
+    return this._query;
+}
 
-jsUri.prototype.__defineGetter__('query', function () { return this._query; });
-jsUri.prototype.__defineSetter__('query', function(val) { this._query = new jsUri.query(val); });
-
-jsUri.prototype.__defineGetter__('anchor', function () { return this._uri.anchor; });
-jsUri.prototype.__defineSetter__('anchor', function (val) { this._uri.anchor = val; });
+jsUri.prototype.anchor = function(val) {
+    if (typeof val != 'undefined') {
+        this._uri.anchor = val;
+    }
+    return this._uri.anchor;
+}
 
 
 /*
@@ -162,42 +195,42 @@ jsUri.prototype.__defineSetter__('anchor', function (val) { this._uri.anchor = v
 */
 
 jsUri.prototype.setProtocol = function (val) {
-    this.protocol = val;
+    this.protocol(val);
     return this;
 }
 
 jsUri.prototype.setHasAuthorityPrefix = function (val) {
-    this.hasAuthorityPrefix = val;
+    this.hasAuthorityPrefix(val);
     return this;
 }
 
 jsUri.prototype.setUserInfo = function (val) {
-    this.userInfo = val;
+    this.userInfo(val);
     return this;
 }
 
 jsUri.prototype.setHost = function (val) {
-    this.host = val;
+    this.host(val);
     return this;
 }
 
 jsUri.prototype.setPort = function (val) {
-    this.port = val;
+    this.port(val);
     return this;
 }
 
 jsUri.prototype.setPath = function (val) {
-    this.path = val;
+    this.path(val);
     return this;
 }
 
 jsUri.prototype.setQuery = function (val) {
-    this.query = val;
+    this.query(val);
     return this;
 }
 
 jsUri.prototype.setAnchor = function (val) {
-    this.anchor = val;
+    this.anchor(val);
     return this;
 }
 
@@ -254,9 +287,9 @@ jsUri.query.prototype.decode = function (s) {
 
 // getQueryParamValues(key) returns the first query param value found for the key 'key'
 jsUri.prototype.getQueryParamValue = function (key) {
-    for (var p in this.query.params) {
-        var param = this.query.params[p];
-        if (this.query.decode(key) == this.query.decode(param[0]))
+    for (var p in this.query().params) {
+        var param = this.query().params[p];
+        if (this.query().decode(key) == this.query().decode(param[0]))
             return param[1];
     }
 }
@@ -264,9 +297,9 @@ jsUri.prototype.getQueryParamValue = function (key) {
 // getQueryParamValues(key) returns an array of query param values for the key 'key'
 jsUri.prototype.getQueryParamValues = function (key) {
     var arr = [];
-    for (var p in this.query.params) {
-        var param = this.query.params[p];
-        if (this.query.decode(key) == this.query.decode(param[0]))
+    for (var p in this.query().params) {
+        var param = this.query().params[p];
+        if (this.query().decode(key) == this.query().decode(param[0]))
             arr.push(param[1]);
     }
     return arr;
@@ -278,17 +311,17 @@ jsUri.prototype.deleteQueryParam = function (key, val) {
 
     var arr = [];
 
-    for (var p in this.query.params) {
-        var param = this.query.params[p];
-        if (arguments.length == 2 && this.query.decode(param[0]) == this.query.decode(key) && this.query.decode(param[1]) == this.query.decode(val))
+    for (var p in this.query().params) {
+        var param = this.query().params[p];
+        if (arguments.length == 2 && this.query().decode(param[0]) == this.query().decode(key) && this.query().decode(param[1]) == this.query().decode(val))
             continue;
-        else if (arguments.length == 1 && this.query.decode(param[0]) == this.query.decode(key))
+        else if (arguments.length == 1 && this.query().decode(param[0]) == this.query().decode(key))
             continue;
 
         arr.push(param);
     }
 
-    this.query.params = arr;
+    this.query().params = arr;
     return this;
 }
 
@@ -297,11 +330,11 @@ jsUri.prototype.deleteQueryParam = function (key, val) {
 jsUri.prototype.addQueryParam = function (key, val, index) {
 
     if (arguments.length == 3 && index != -1) {
-        index = Math.min(index, this.query.params.length);
-        this.query.params.splice(index, 0, [key, val]);
+        index = Math.min(index, this.query().params.length);
+        this.query().params.splice(index, 0, [key, val]);
     }
     else if (arguments.length > 0)
-        this.query.params.push([key, val]);
+        this.query().params.push([key, val]);
     
     return this;
 }
@@ -313,9 +346,9 @@ jsUri.prototype.replaceQueryParam = function (key, newVal, oldVal) {
 
     if (arguments.length == 3) {
         var index = -1;
-        for (var p in this.query.params) {
-            var param = this.query.params[p];
-            if (this.query.decode(param[0]) == this.query.decode(key) && decodeURIComponent(param[1]) == this.query.decode(oldVal)) {
+        for (var p in this.query().params) {
+            var param = this.query().params[p];
+            if (this.query().decode(param[0]) == this.query().decode(key) && decodeURIComponent(param[1]) == this.query().decode(oldVal)) {
                 index = p;
                 break;
             }
@@ -324,9 +357,9 @@ jsUri.prototype.replaceQueryParam = function (key, newVal, oldVal) {
     }
     else {
         var index = -1;
-        for (var p in this.query.params) {
-            var param = this.query.params[p];
-            if (this.query.decode(param[0]) == this.query.decode(key)) {
+        for (var p in this.query().params) {
+            var param = this.query().params[p];
+            if (this.query().decode(param[0]) == this.query().decode(key)) {
                 index = p;
                 break;
             }
