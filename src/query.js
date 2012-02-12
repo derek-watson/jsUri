45f6db0d8650e3cf60b394a85184f7579fc10837
Derek Watson <watson@dcw.ca>
2012-02-12 10:02:37 -0500

    refactored query.js using prototype style

--- src/query.js
+++ src/query.js
@@ -1,157 +1,144 @@
+var Query = (function () {
 
-var Query = function (queryString) {
+  'use strict';
 
-    // query string parsing, parameter manipulation and stringification
+  /*jslint white:true, plusplus: true */
 
-    'use strict';
+  function decode(s) {
+    s = decodeURIComponent(s);
+    s = s.replace('+', ' ');
+    return s;
+  }
+ 
+  function Query(q) {
+    var i, ps, p, kvp, k, v;
 
-    var // parseQuery(q) parses the uri query string and returns a multi-dimensional array of the components
-        parseQuery = function (q) {
-            var arr = [], i, ps, p, kvp, k, v;
+    this.params = [];
 
-            if (typeof (q) === 'undefined' || q === null || q === '') {
-                return arr;
-            }
+    if (typeof (q) === 'undefined' || q === null || q === '') {
+      return;
+    }
 
-            if (q.indexOf('?') === 0) {
-                q = q.substring(1);
-            }
+    if (q.indexOf('?') === 0) {
+      q = q.substring(1);
+    }
 
-            ps = q.toString().split(/[&;]/);
+    ps = q.toString().split(/[&;]/);
 
-            for (i = 0; i < ps.length; i++) {
-                p = ps[i];
-                kvp = p.split('=');
-                k = kvp[0];
-                v = p.indexOf('=') === -1 ? null : (kvp[1] === null ? '' : kvp[1]);
-                arr.push([k, v]);
-            }
+    for (i = 0; i < ps.length; i++) {
+      p = ps[i];
+      kvp = p.split('=');
+      k = kvp[0];
+      v = p.indexOf('=') === -1 ? null : (kvp[1] === null ? '' : kvp[1]);
+      this.params.push([k, v]);
+    }
+  }
 
-            return arr;
-        },
+  // getParamValues(key) returns the first query param value found for the key 'key'
+  Query.prototype.getParamValue = function (key) {
+    var param, i;
+    for (i = 0; i < this.params.length; i++) {
+      param = this.params[i];
+      if (decode(key) === decode(param[0])) {
+        return param[1];
+      }
+    }
+  };
 
-        params = parseQuery(queryString),
+  // getParamValues(key) returns an array of query param values for the key 'key'
+  Query.prototype.getParamValues = function (key) {
+    var arr = [], i, param;
+    for (i = 0; i < this.params.length; i++) {
+      param = this.params[i];
+      if (decode(key) === decode(param[0])) {
+        arr.push(param[1]);
+      }
+    }
+    return arr;
+  };
 
-        // toString() returns a string representation of the internal state of the object
-        toString = function () {
-            var s = '', i, param;
-            for (i = 0; i < params.length; i++) {
-                param = params[i];
-                if (s.length > 0) {
-                    s += '&';
-                }
-                if (param[1] === null) {
-                  s += param[0];
-                }
-                else {
-                  s += param.join('=');
-                }
-            }
-            return s.length > 0 ? '?' + s : s;
-        },
+  // deleteParam(key) removes all instances of parameters named (key)
+  // deleteParam(key, val) removes all instances where the value matches (val)
+  Query.prototype.deleteParam = function (key, val) {
 
-        decode = function (s) {
-            s = decodeURIComponent(s);
-            s = s.replace('+', ' ');
-            return s;
-        },
+    var arr = [], i, param, keyMatchesFilter, valMatchesFilter;
 
-        // getParamValues(key) returns the first query param value found for the key 'key'
-        getParamValue = function (key) {
-            var param, i;
-            for (i = 0; i < params.length; i++) {
-                param = params[i];
-                if (decode(key) === decode(param[0])) {
-                    return param[1];
-                }
-            }
-        },
+    for (i = 0; i < this.params.length; i++) {
 
-        // getParamValues(key) returns an array of query param values for the key 'key'
-        getParamValues = function (key) {
-            var arr = [], i, param;
-            for (i = 0; i < params.length; i++) {
-                param = params[i];
-                if (decode(key) === decode(param[0])) {
-                    arr.push(param[1]);
-                }
-            }
-            return arr;
-        },
+      param = this.params[i];
+      keyMatchesFilter = decode(param[0]) === decode(key);
+      valMatchesFilter = decode(param[1]) === decode(val);
 
-        // deleteParam(key) removes all instances of parameters named (key)
-        // deleteParam(key, val) removes all instances where the value matches (val)
-        deleteParam = function (key, val) {
+      if ((arguments.length === 1 && !keyMatchesFilter) || 
+          (arguments.length === 2 && !keyMatchesFilter && !valMatchesFilter)) {
+        arr.push(param);
+      }
+    }
 
-            var arr = [], i, param, keyMatchesFilter, valMatchesFilter;
+    this.params = arr;
 
-            for (i = 0; i < params.length; i++) {
+    return this;
+  };
 
-                param = params[i];
-                keyMatchesFilter = decode(param[0]) === decode(key);
-                valMatchesFilter = decode(param[1]) === decode(val);
+  // addParam(key, val) Adds an element to the end of the list of query parameters
+  // addParam(key, val, index) adds the param at the specified position (index)
+  Query.prototype.addParam = function (key, val, index) {
 
-                if ((arguments.length === 1 && !keyMatchesFilter) || (arguments.length === 2 && !keyMatchesFilter && !valMatchesFilter)) {
-                    arr.push(param);
-                }
-            }
+    if (arguments.length === 3 && index !== -1) {
+      index = Math.min(index, this.params.length);
+      this.params.splice(index, 0, [key, val]);
+    } else if (arguments.length > 0) {
+      this.params.push([key, val]);
+    }
+    return this;
+  };
 
-            params = arr;
+  // replaceParam(key, newVal) deletes all instances of params named (key) and replaces them with the new single value
+  // replaceParam(key, newVal, oldVal) deletes only instances of params named (key) with the value (val) and replaces them with the new single value
+  // this function attempts to preserve query param ordering
+  Query.prototype.replaceParam = function (key, newVal, oldVal) {
 
-            return this;
-        },
+    var index = -1, i, param;
 
-        // addParam(key, val) Adds an element to the end of the list of query parameters
-        // addParam(key, val, index) adds the param at the specified position (index)
-        addParam = function (key, val, index) {
+    if (arguments.length === 3) {
+      for (i = 0; i < this.params.length; i++) {
+        param = this.params[i];
+        if (decode(param[0]) === decode(key) && decodeURIComponent(param[1]) === decode(oldVal)) {
+          index = i;
+          break;
+        }
+      }
+      this.deleteParam(key, oldVal).addParam(key, newVal, index);
+    } else {
+      for (i = 0; i < this.params.length; i++) {
+        param = this.params[i];
+        if (decode(param[0]) === decode(key)) {
+          index = i;
+          break;
+        }
+      }
+      this.deleteParam(key);
+      this.addParam(key, newVal, index);
+    }
+    return this;
+  };
 
-            if (arguments.length === 3 && index !== -1) {
-                index = Math.min(index, params.length);
-                params.splice(index, 0, [key, val]);
-            } else if (arguments.length > 0) {
-                params.push([key, val]);
-            }
-            return this;
-        },
+  Query.prototype.toString = function () {
+    var s = '', i, param;
+    for (i = 0; i < this.params.length; i++) {
+      param = this.params[i];
+      if (s.length > 0) {
+        s += '&';
+      }
+      if (param[1] === null) {
+        s += param[0];
+      }
+      else {
+        s += param.join('=');
+      }
+    }
+    return s.length > 0 ? '?' + s : s;
+  };
 
-        // replaceParam(key, newVal) deletes all instances of params named (key) and replaces them with the new single value
-        // replaceParam(key, newVal, oldVal) deletes only instances of params named (key) with the value (val) and replaces them with the new single value
-        // this function attempts to preserve query param ordering
-        replaceParam = function (key, newVal, oldVal) {
-
-            var index = -1, i, param;
-
-            if (arguments.length === 3) {
-                for (i = 0; i < params.length; i++) {
-                    param = params[i];
-                    if (decode(param[0]) === decode(key) && decodeURIComponent(param[1]) === decode(oldVal)) {
-                        index = i;
-                        break;
-                    }
-                }
-                deleteParam(key, oldVal).addParam(key, newVal, index);
-            } else {
-                for (i = 0; i < params.length; i++) {
-                    param = params[i];
-                    if (decode(param[0]) === decode(key)) {
-                        index = i;
-                        break;
-                    }
-                }
-                deleteParam(key);
-                addParam(key, newVal, index);
-            }
-            return this;
-        };
-
-    // public api
-    return {
-        getParamValue: getParamValue,
-        getParamValues: getParamValues,
-        deleteParam: deleteParam,
-        addParam: addParam,
-        replaceParam: replaceParam,
-        
-        toString: toString
-    };
-};
+  return Query;
+}());
