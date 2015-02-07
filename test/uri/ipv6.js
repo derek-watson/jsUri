8e2b1494172a6b6b97e47336ce71304d1b282842
Martin Panel <martin@xorax.info>
2015-02-07 21:55:02 +0100

    use Chai for the tests

--- test/uri/ipv6.js
+++ test/uri/ipv6.js
@@ -1,33 +1,33 @@
-var assert = require('assert');
+var expect = require('chai').expect
 
 var Uri = (typeof(require) === 'function') ? require('../../Uri') : window.Uri
 
 describe('Uri', function() {
 
   it('understand ipv6 host', function (){
-    assert.equal(new Uri('https://[2001:db8:85a3:8d3:1319:8a2e:370:7348]:12:').toString(), 'https://[2001:db8:85a3:8d3:1319:8a2e:370:7348]:12')
-    assert.equal(new Uri('https://[::ffff:192.169.0.1]:12:').toString(), 'https://[::ffff:192.169.0.1]:12')
-    assert.equal(new Uri('https://[2001:db8::68]:12:').toString(), 'https://[2001:db8::68]:12')
+    expect(new Uri('https://[2001:db8:85a3:8d3:1319:8a2e:370:7348]:12:').toString()).to.equal('https://[2001:db8:85a3:8d3:1319:8a2e:370:7348]:12')
+    expect(new Uri('https://[::ffff:192.169.0.1]:12:').toString()).to.equal('https://[::ffff:192.169.0.1]:12')
+    expect(new Uri('https://[2001:db8::68]:12:').toString()).to.equal('https://[2001:db8::68]:12')
   })
 
   it('should correct host and port on ipv6', function (){
     u = new Uri('ssh://[2001:db8:85a3:8d3:1319:8a2e:370:7348]')
     u.setHost('[2001:db8:85a3:8d3:1319:8a2e:370:73ff]');
     u.setPort(12);
-    assert.equal(u.toString(), 'ssh://[2001:db8:85a3:8d3:1319:8a2e:370:73ff]:12')
+    expect(u.toString()).to.equal('ssh://[2001:db8:85a3:8d3:1319:8a2e:370:73ff]:12')
   })
 
   it('should correct host and port on ipv6 colon uri', function (){
     u = new Uri('ssh://[2001:db8:85a3:8d3:1319:8a2e:370:7348]:')
     u.setHost('[2001:db8:85a3:8d3:1319:8a2e:370:73ff]');
     u.setPort(12);
-    assert.equal(u.toString(), 'ssh://[2001:db8:85a3:8d3:1319:8a2e:370:73ff]:12')
+    expect(u.toString()).to.equal('ssh://[2001:db8:85a3:8d3:1319:8a2e:370:73ff]:12')
   })
 
   it('should correctly set path on ipv6 uri', function (){
     u = new Uri('https://[2001:db8:85a3:8d3:1319:8a2e:370:7348]:12')
     u.setPath('relativePath')
-    assert.equal(u.toString(), 'https://[2001:db8:85a3:8d3:1319:8a2e:370:7348]:12/relativePath')
+    expect(u.toString()).to.equal('https://[2001:db8:85a3:8d3:1319:8a2e:370:7348]:12/relativePath')
   })
 
   it('should correctly host, port and path on ipv6 colon uri', function (){
@@ -35,12 +35,12 @@ describe('Uri', function() {
     u.setHost('[2001:db8:85a3:8d3:1319:8a2e:370:73ff]');
     u.setPort(12);
     u.setPath('relativePath')
-    assert.equal(u.toString(), 'ssh://[2001:db8:85a3:8d3:1319:8a2e:370:73ff]:12:relativePath')
+    expect(u.toString()).to.equal('ssh://[2001:db8:85a3:8d3:1319:8a2e:370:73ff]:12:relativePath')
   })
 
   it('should correctly set path on ipv6 colon uri', function (){
     u = new Uri('https://[2001:db8:85a3:8d3:1319:8a2e:370:7348]:12:')
     u.setPath('relativePath')
-    assert.equal(u.toString(), 'https://[2001:db8:85a3:8d3:1319:8a2e:370:7348]:12:relativePath')
+    expect(u.toString()).to.equal('https://[2001:db8:85a3:8d3:1319:8a2e:370:7348]:12:relativePath')
   })
 })
